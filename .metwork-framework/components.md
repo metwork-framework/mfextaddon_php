01097fa0c1dbc8a11d86a4b1555a70a3c4ba2591
thebaptiste <jean-baptiste.veslin@meteo.fr>
2020-03-05 18:23:38 +0100

    feat: first version with php build

---
+++ .metwork-framework/components.md
@@ -0,0 +1,5 @@
+| Name | Version | Layer |
+| --- | --- | --- |
+| [php](https://www.php.net/) | 7.4.3 | php |
+
+*(1 component)*
---
+++ layers/layer1_php/0010_php/Makefile.mk
@@ -0,0 +1,16 @@
+include ../../../adm/root.mk
+include $(MFEXT_HOME)/share/package.mk
+
+export NAME=php
+export VERSION=7.4.3
+export EXTENSION=tar.xz
+export CHECKTYPE=MD5
+export CHECKSUM=853df4727b2dcde52e9cc9d944f87498
+DESCRIPTION=\
+PHP is a popular general-purpose scripting language that is especially suited to web development
+WEBSITE=https://www.php.net/
+LICENSE=PHP license (BSD-style license without "copyleft" restrictions associated with GPL)
+
+all:: $(PREFIX)/bin/php
+$(PREFIX)/bin/php:
+	$(MAKE) --file=$(MFEXT_HOME)/share/Makefile.standard PREFIX=$(PREFIX) OPTIONS="--with-curl --with-readline --enable-fpm --enable-cgi" download uncompress configure build install
