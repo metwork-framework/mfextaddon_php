db71466976e59a6389a2271ef5004a787fa4ec2b
Matthieu MARRAST <matthieu.marrast@gmail.com>
2021-08-05 16:45:07 +0200

    feat: initialization of PHP configuration files to work with mfserv

--- layers/layer1_php/0010_php/Makefile.mk
+++ layers/layer1_php/0010_php/Makefile.mk
@@ -14,3 +14,20 @@ LICENSE=PHP license (BSD-style license without "copyleft" restrictions associate
 all:: $(PREFIX)/bin/php
 $(PREFIX)/bin/php:
 	$(MAKE) --file=$(MFEXT_HOME)/share/Makefile.standard PREFIX=$(PREFIX) OPTIONS="--with-curl --with-readline=$(PREFIX)/../core --with-openssl --with-pgsql=$(PREFIX)/../scientific_core --enable-fpm --enable-cgi" download uncompress configure build install
+
+	# init config files
+	cp ./build/php-$(VERSION)/php.ini-development $(PREFIX)/lib/php.ini
+	mv $(PREFIX)/etc/php-fpm.conf.default $(PREFIX)/etc/php-fpm.conf
+	mv $(PREFIX)/etc/php-fpm.d/www.conf.default $(PREFIX)/etc/php-fpm.d/www.conf
+
+	# configure php-fpm
+	sed -i 's/^user = nobody/;user = nobody/g' $(PREFIX)/etc/php-fpm.d/www.conf
+	sed -i 's/^group = nobody/;group = nobody/g' $(PREFIX)/etc/php-fpm.d/www.conf
+	sed -i 's/^listen = .*/listen = {{MFMODULE_RUNTIME_HOME}}\/var\/php-fpm.sock/g' $(PREFIX)/etc/php-fpm.d/www.conf
+	sed -i 's/^;listen.owner = .*/listen.owner = {{MFMODULE_RUNTIME_USER}}/g' $(PREFIX)/etc/php-fpm.d/www.conf
+	sed -i 's/^;listen.group = .*/listen.group = {{MFMODULE_RUNTIME_GROUP}}/g' $(PREFIX)/etc/php-fpm.d/www.conf
+	sed -i 's/^;listen.mode = .*/listen.mode = 0660/g' $(PREFIX)/etc/php-fpm.d/www.conf
+	sed -i 's/%{%Y-%m-%dT%H:%M:%S%z}/{% raw %}%{%Y-%m-%dT%H:%M:%S%z}{% endraw %}/g' $(PREFIX)/etc/php-fpm.d/www.conf
+	sed -i 's/^;error_log = .*/error_log = {{MFMODULE_RUNTIME_HOME}}\/log\/php-fpm.log/g' $(PREFIX)/etc/php-fpm.conf
+	sed -i 's/^include=.*/include = {{WWWCONF}}/g' $(PREFIX)/etc/php-fpm.conf
+
