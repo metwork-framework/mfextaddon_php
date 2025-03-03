bef6de91376431ade25d6fcf67e57ca7af51559d
thebaptiste <jean-baptiste.veslin@meteo.fr>
2025-03-03 11:57:01 +0000

    build: clear_env must be at no

--- layers/layer1_php/0010_php/Makefile.mk
+++ layers/layer1_php/0010_php/Makefile.mk
@@ -28,6 +28,7 @@ $(PREFIX)/bin/php:
 	sed -i 's/^;listen.owner = .*/listen.owner = {{MFMODULE_RUNTIME_USER}}/g' $(PREFIX)/etc/php-fpm.d/www.conf
 	sed -i 's/^;listen.group = .*/listen.group = {{MFMODULE_RUNTIME_GROUP}}/g' $(PREFIX)/etc/php-fpm.d/www.conf
 	sed -i 's/^;listen.mode = .*/listen.mode = 0660/g' $(PREFIX)/etc/php-fpm.d/www.conf
+	sed -i 's/^;clear_env = no/clear_env = no/g' $(PREFIX)/etc/php-fpm.d/www.conf
 	sed -i 's/%{%Y-%m-%dT%H:%M:%S%z}/{% raw %}%{%Y-%m-%dT%H:%M:%S%z}{% endraw %}/g' $(PREFIX)/etc/php-fpm.d/www.conf
 	sed -i 's/^;error_log = .*/error_log = {{PHP_LOGFILE_PATH}}/g' $(PREFIX)/etc/php-fpm.conf
 	sed -i 's/^include=.*/include = {{WWWCONF}}/g' $(PREFIX)/etc/php-fpm.conf
